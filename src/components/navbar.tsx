ddfe05e2f7cb218e77c97a556f364260b472c015
Devedu <deveduar@gmail.com>
2024-07-27 16:27:52 +0200

    useeffect fixed in navbar and hadleClickOutside ,follow eslint rule in about

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Link from "next/link";
 import ThemeSwitch from "./themeswitch";
 
@@ -11,39 +11,44 @@ const Navbar: React.FC = () => {
     setIsOpen(!isOpen);
   };
 
-  const closeMenu = () => {
+  const closeMenu = useCallback(() => {
     setIsOpen(false);
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    const menu = document.getElementById("navbar-default");
-    const button = document.querySelector("[data-collapse-toggle]");
-
-    if (menu && !menu.contains(event.target as Node) && button && !button.contains(event.target as Node)) {
-      closeMenu();
-    }
-  };
+  }, []);
 
   useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const menu = document.getElementById("navbar-default");
+      const button = document.querySelector("[data-collapse-toggle]");
+
+      if (menu && !menu.contains(event.target as Node) && button && !button.contains(event.target as Node)) {
+        closeMenu();
+      }
+    };
+
     if (isOpen) {
       document.addEventListener("click", handleClickOutside);
     } else {
       document.removeEventListener("click", handleClickOutside);
     }
+
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
-  }, [isOpen]);
+  }, [isOpen, closeMenu]);
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 "> 
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 lg:px-24 relative">
         <div className="flex items-center md:space-x-8 lg:space-x-72 xl:space-x-96">
           <Link href="/" passHref className="flex items-center space-x-3 rtl:space-x-reverse pr-20">
             <span className="self-center text-2xl font-bold whitespace-nowrap text-gray-900 dark:text-white">Deveduar</span>
           </Link>
-          <div className={`${isOpen ? "block" : "hidden"} absolute top-16 left-0 right-0 bg-white bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-50 md:static md:block md:w-auto md:bg-transparent dark:md:bg-transparent transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`} id="navbar-default">
-            
+          <div
+            className={`${isOpen ? "block" : "hidden"} absolute top-16 left-0 right-0 bg-white bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-50 md:static md:block md:w-auto md:bg-transparent dark:md:bg-transparent transform transition-transform duration-300 ${
+              isOpen ? "translate-x-0" : "-translate-x-full"
+            } md:translate-x-0`}
+            id="navbar-default"
+          >
             <ul className="font-medium flex flex-col p-3 md:p-0 mt-2 md:mt-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:border-0 bg-gray-50 bg-opacity-90 md:bg-transparent dark:bg-gray-800 dark:bg-opacity-90 md:dark:bg-transparent dark:border-gray-700">
               <li>
                 <Link href="/" passHref className="block py-2 px-3 text-gray-800 rounded md:bg-transparent md:text-gray-900 md:p-0 dark:text-white" onClick={closeMenu}>
@@ -69,7 +74,7 @@ const Navbar: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex items-center space-x-3 ml-auto"> 
+        <div className="flex items-center space-x-3 ml-auto">
           <button
             data-collapse-toggle="navbar-default"
             type="button"
